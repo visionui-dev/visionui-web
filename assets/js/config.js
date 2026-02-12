e0bf37c9f0b9361cae5aac9c10568756cd859193
visionui-dev <visionui.dev@gmail.com>
2026-02-12 16:36:28 -0300

    Update - assets/js/config.js

---
+++ assets/js/config.js
@@ -0,0 +1,19 @@
+/**
+ * VisionUI Website configuration
+ *
+ * Central place to set external URLs without editing HTML.
+ * Keep values empty ("") to disable optional links.
+ */
+
+window.VISIONUI_LINKS = {
+    demoDownloadUrl: 'pages/store.html',
+    demoVideoUrl: '',
+
+    websiteUrl: 'https://visionui.app',
+    discordUrl: 'https://discord.gg/visionui',
+
+    xUrl: '',
+    youtubeUrl: '',
+    tiktokUrl: '',
+    githubUrl: ''
+};
